fc6ed391b632132de3ddea827935a4daf9cbaaa8
Dalton Dick <daltondick2010@gmail.com>
2012-11-26 23:16:20 -0600

    Added some timing of the downloads

--- program.py
+++ program.py
@@ -1,10 +1,15 @@
 import reddit
+import time
 
 sub = raw_input("What subreddit would you like?\n")
 pages = raw_input("How many pages?\n")
 
 sub = reddit.Reddit(sub, pages)
+x = time.clock()
 sub.downloadImagesAsync()
+y = time.clock()
+
+print y - x
 
 print "Downloaded " + str(sub.IMGS) + " images"
 print "Downloaded " + str(sub.ALBUMS) + " albums"
