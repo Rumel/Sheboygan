4bb8a1d90e3d1c14401479171ff39f790e50b579
Dalton Dick <daltondick2010@gmail.com>
2012-12-16 17:05:55 -0600

    Converted tabs to spaces

--- imgur.py
+++ imgur.py
@@ -4,115 +4,116 @@ import urllib
 import sys
 import threading
 
+
 def formatNumber(num):
-	if(num < 10):
-		return "00" + str(num)
-	elif(num < 100):
-		return "0" + str(num)
-	else:
-		return str(num)
+    if(num < 10):
+        return "00" + str(num)
+    elif(num < 100):
+        return "0" + str(num)
+    else:
+        return str(num)
 
 def getJson(jsonUrl):
-	try:
-		u = urllib.urlopen(jsonUrl)
-		return json.load(u)
-	except:
-		sys.stdout.write("Error occured on getting json\n")
-		return 0
+    try:
+        u = urllib.urlopen(jsonUrl)
+        return json.load(u)
+    except:
+        sys.stdout.write("Error occured on getting json\n")
+        return 0
 
 class Imgur:
-	def __init_(self):
+    def __init_(self):
 
-		return
+        return
 
-	def directlyDownload(self, url, sub, album = "", count = 0):
-		split = url.split("/")
-		last = split[len(split) - 1]
-		ext = last.split(".")[1]
-		if(ext != "gif"):
-			img = ".\\downloads\\" + sub + "\\img\\"
-			if(album != ""):
-				save = img + album + " - " + formatNumber(count) + "." + ext
-			else:
-				save = img + last
-		else:
-			gif = ".\\downloads\\" + sub + "\\gif\\"
-			if(album != ""):
-				save = gif + album + " - "  + formatNumber(count) + ".gif"
-			else:
-				save = gif + last
+    def directlyDownload(self, url, sub, album = "", count = 0):
+        split = url.split("/")
+        last = split[len(split) - 1]
+        ext = last.split(".")[1]
+        if(ext != "gif"):
+            img = ".\\downloads\\" + sub + "\\img\\"
+            if(album != ""):
+                save = img + album + " - " + formatNumber(count) + "." + ext
+            else:
+                save = img + last
+        else:
+            gif = ".\\downloads\\" + sub + "\\gif\\"
+            if(album != ""):
+                save = gif + album + " - "  + formatNumber(count) + ".gif"
+            else:
+                save = gif + last
 
-		if(not os.path.exists(save)):
-			try:
-				urllib.urlretrieve(url, save)
-			except IOError:
-				sys.stdout.write("Imgur didn't respond\n")
-			sys.stdout.write('Downloaded ' + save + '\n')
-		else:
-			sys.stdout.write('Skipping already downloaded\n')
+        if(not os.path.exists(save)):
+            try:
+                urllib.urlretrieve(url, save)
+            except IOError:
+                sys.stdout.write("Imgur didn't respond\n")
+            sys.stdout.write('Downloaded ' + save + '\n')
+        else:
+            sys.stdout.write('Skipping already downloaded\n')
 
-	def downloadAlbum(self, url, sub):
-		base = "http://api.imgur.com/2/album/"
-		ending = url.split("/")[4]
-		jsonUrl = base + ending + ".json"
-		j = getJson(jsonUrl)
-		if(j == 0):
-			return 0
+    def downloadAlbum(self, url, sub):
+        base = "http://api.imgur.com/2/album/"
+        ending = url.split("/")[4]
+        jsonUrl = base + ending + ".json"
+        j = getJson(jsonUrl)
+        if(j == 0):
+            return 0
 
-		#Check to see if the album exists
-		for d in j:
-			if(d == u'error'):
-				sys.stdout.wrte('There was an error finding the album\n')
-				return 0
+        #Check to see if the album exists
+        for d in j:
+            if(d == u'error'):
+                sys.stdout.wrte('There was an error finding the album\n')
+                return 0
 
-		images = j[u'album'][u'images']
-		count = 0
-		for i in images:
-			download = i[u'links'][u'original']
-			self.directlyDownload(download, sub, album = ending, count = count)
-			count = count + 1
-		return count
+        images = j[u'album'][u'images']
+        count = 0
+        for i in images:
+            download = i[u'links'][u'original']
+            self.directlyDownload(download, sub, album = ending, count = count)
+            count = count + 1
+        return count
 
-	def downloadAlbumAsync(self, url, sub):
-		base = "http://api.imgur.com/2/album/"
-		ending = url.split("/")[4]
-		jsonUrl = base + ending + ".json"
-		j = getJson(jsonUrl)
-		if(j == 0):
-			return 0
+    def downloadAlbumAsync(self, url, sub):
+        base = "http://api.imgur.com/2/album/"
+        ending = url.split("/")[4]
+        jsonUrl = base + ending + ".json"
+        j = getJson(jsonUrl)
+        if(j == 0):
+            return 0
 
-		#Check to see if the album exists
-		for d in j:
-			if(d == u'error'):
-				sys.stdout.wrte('There was an error finding the album\n')
-				return 0
+        #Check to see if the album exists
+        for d in j:
+            if(d == u'error'):
+                sys.stdout.wrte('There was an error finding the album\n')
+                return 0
 
-		images = j[u'album'][u'images']
-		count = 0
-		threads = []
-		for i in images:
-			count = count + 1
-			download = i[u'links'][u'original']
-			thread = threading.Thread(target=self.directlyDownload, args=([download, sub, ending, count]))
-			threads.append(thread)
-		for t in threads:
-			t.start()
-		for t in threads:
-			t.join()
-		return count
+        images = j[u'album'][u'images']
+        count = 0
+        threads = []
+        for i in images:
+            count = count + 1
+            download = i[u'links'][u'original']
+            thread = threading.Thread(target=self.directlyDownload, args=([download, sub, ending, count]))
+            threads.append(thread)
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        return count
 
-	def downloadImage(self, url, sub):
-		ending = url.split("/")[3]
-		base = "http://api.imgur.com/2/image/"
-		jsonUrl = base + ending + ".json"
-		try:
-			j = getJson(jsonUrl)
-			if(j == 0):
-				return
-			link = j[u'image'][u'links'][u'original']
-			self.directlyDownload(link, sub)
-		except ValueError:
-			sys.stdout.write(url + '\n' + jsonUrl + '\nNo JSON object could be decoded\n')
-		except:
-			sys.stdout.write('Unkown Error\n')
-		return
+    def downloadImage(self, url, sub):
+        ending = url.split("/")[3]
+        base = "http://api.imgur.com/2/image/"
+        jsonUrl = base + ending + ".json"
+        try:
+            j = getJson(jsonUrl)
+            if(j == 0):
+                return
+            link = j[u'image'][u'links'][u'original']
+            self.directlyDownload(link, sub)
+        except ValueError:
+            sys.stdout.write(url + '\n' + jsonUrl + '\nNo JSON object could be decoded\n')
+        except:
+            sys.stdout.write('Unkown Error\n')
+        return
--- reddit.py
+++ reddit.py
@@ -7,147 +7,147 @@ import threading
 import sys
 
 def getOpener():
-	opener = urllib2.build_opener()
-	opener.addheaders = [('User-Agent', 'Sheboygan Reddit Downloader')]
-	return opener
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', 'Sheboygan Reddit Downloader')]
+    return opener
 
 def buildUrl(base, sub):
-	return base + sub + ".json"
+    return base + sub + ".json"
 
 def createIfNotExists(dir):
-	if(not os.path.exists(dir)):
-		os.mkdir(dir)
-		print "Created " + dir
-	return
+    if(not os.path.exists(dir)):
+        os.mkdir(dir)
+        print "Created " + dir
+    return
 
 def makeDir(dir):
-	dls = ".\\downloads\\"
-	img = "\\img"
-	gif = "\\gif"
-	dlDir = dls + dir
-	dlDirImg = dlDir + img
-	dlDirGif = dlDir + gif
-	createIfNotExists(dls)
-	createIfNotExists(dlDir)
-	createIfNotExists(dlDirImg)
-	createIfNotExists(dlDirGif)
+    dls = ".\\downloads\\"
+    img = "\\img"
+    gif = "\\gif"
+    dlDir = dls + dir
+    dlDirImg = dlDir + img
+    dlDirGif = dlDir + gif
+    createIfNotExists(dls)
+    createIfNotExists(dlDir)
+    createIfNotExists(dlDirImg)
+    createIfNotExists(dlDirGif)
 
 class Reddit:
-	BASE = "http://www.reddit.com/r/"
-	URL = ""
-	LINKS = []
-	SUB = ""
-	IMGS = 0
-	ALBUMS = 0
+    BASE = "http://www.reddit.com/r/"
+    URL = ""
+    LINKS = []
+    SUB = ""
+    IMGS = 0
+    ALBUMS = 0
 
-	def __init__(self, sub, pages = 1):
-		self.SUB = sub
-		self.URL = buildUrl(self.BASE, self.SUB)
-		self.getLinks(pages)
-		return
+    def __init__(self, sub, pages = 1):
+        self.SUB = sub
+        self.URL = buildUrl(self.BASE, self.SUB)
+        self.getLinks(pages)
+        return
 
-	def getLinks(self, pages = 1):
-		print "This will take approximately " + str(int(pages) * 2) + " seconds."
-		print "This is due to Reddit's two second API rule"
-		opener = getOpener()
-		after = ""
-		for i in range(0, int(pages)):
-			print "Getting page " + str(i + 1)
-			if(i != 0):
-				u = opener.open(self.URL + "?after=" + after)
-			else:
-				u = opener.open(self.URL)
-			page = json.load(u)
-			children = page[u'data'][u'children']
-			for l in children:
-				self.LINKS.append(Link(l))
-			after = page[u'data'][u'after']
-			if(after is None):
-				break
-		time.sleep(2)
+    def getLinks(self, pages = 1):
+        print "This will take approximately " + str(int(pages) * 2) + " seconds."
+        print "This is due to Reddit's two second API rule"
+        opener = getOpener()
+        after = ""
+        for i in range(0, int(pages)):
+            print "Getting page " + str(i + 1)
+            if(i != 0):
+                u = opener.open(self.URL + "?after=" + after)
+            else:
+                u = opener.open(self.URL)
+            page = json.load(u)
+            children = page[u'data'][u'children']
+            for l in children:
+                self.LINKS.append(Link(l))
+            after = page[u'data'][u'after']
+            if(after is None):
+                break
+        time.sleep(2)
 
 
-	def downloadImage(self, iurl):
-		i = imgur.Imgur()
-		split = iurl.split(".")
-		ext = split[len(split) - 1]
-		if(ext == "jpg" or ext == "gif" or ext == "png"):
-			i.directlyDownload(iurl, self.SUB)
-			self.IMGS = self.IMGS + 1
-		elif(iurl.split("/")[2] == "imgur.com" and iurl.split("/")[3] == "a"):
-			self.IMGS = self.IMGS + i.downloadAlbum(iurl , self.SUB)
-			self.ALBUMS = self.ALBUMS + 1
-		elif(iurl.split("/")[2] == "imgur.com"):
-			i.downloadImage(iurl, self.SUB)
-			self.IMGS = self.IMGS + 1
-		return
+    def downloadImage(self, iurl):
+        i = imgur.Imgur()
+        split = iurl.split(".")
+        ext = split[len(split) - 1]
+        if(ext == "jpg" or ext == "gif" or ext == "png"):
+            i.directlyDownload(iurl, self.SUB)
+            self.IMGS = self.IMGS + 1
+        elif(iurl.split("/")[2] == "imgur.com" and iurl.split("/")[3] == "a"):
+            self.IMGS = self.IMGS + i.downloadAlbum(iurl , self.SUB)
+            self.ALBUMS = self.ALBUMS + 1
+        elif(iurl.split("/")[2] == "imgur.com"):
+            i.downloadImage(iurl, self.SUB)
+            self.IMGS = self.IMGS + 1
+        return
 
-	def downloadImageAsync(self, iurl):
-		i = imgur.Imgur()
-		split = iurl.split(".")
-		ext = split[len(split) - 1]
-		if(ext == "jpg" or ext == "gif" or ext == "png"):
-			i.directlyDownload(iurl, self.SUB)
-			self.IMGS = self.IMGS + 1
-		elif(iurl.split("/")[2] == "imgur.com" and iurl.split("/")[3] == "a"):
-			c = i.downloadAlbumAsync(iurl , self.SUB)
-			self.IMGS = self.IMGS + c
-			self.ALBUMS = self.ALBUMS + 1
-		elif(iurl.split("/")[2] == "imgur.com"):
-			i.downloadImage(iurl, self.SUB)
-			self.IMGS = self.IMGS + 1
-		return
+    def downloadImageAsync(self, iurl):
+        i = imgur.Imgur()
+        split = iurl.split(".")
+        ext = split[len(split) - 1]
+        if(ext == "jpg" or ext == "gif" or ext == "png"):
+            i.directlyDownload(iurl, self.SUB)
+            self.IMGS = self.IMGS + 1
+        elif(iurl.split("/")[2] == "imgur.com" and iurl.split("/")[3] == "a"):
+            c = i.downloadAlbumAsync(iurl , self.SUB)
+            self.IMGS = self.IMGS + c
+            self.ALBUMS = self.ALBUMS + 1
+        elif(iurl.split("/")[2] == "imgur.com"):
+            i.downloadImage(iurl, self.SUB)
+            self.IMGS = self.IMGS + 1
+        return
 
-	def downloadImages(self):
-		makeDir(self.SUB)
-		for i in self.LINKS:
-			self.downloadImage(i.URL)
-		return
+    def downloadImages(self):
+        makeDir(self.SUB)
+        for i in self.LINKS:
+            self.downloadImage(i.URL)
+        return
 
-	def downloadImagesAsync(self):
-		makeDir(self.SUB)
-		threads = []
-		for i in self.LINKS:
-			thread = threading.Thread(target=self.downloadImageAsync, args=([i.URL]))
-			threads.append(thread)
-		for t in threads:
-			t.start()
-		for t in threads:
-			t.join()
-		return
+    def downloadImagesAsync(self):
+        makeDir(self.SUB)
+        threads = []
+        for i in self.LINKS:
+            thread = threading.Thread(target=self.downloadImageAsync, args=([i.URL]))
+            threads.append(thread)
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        return
 
 class Link:
-	def __init__(self, link):
-		self.Kind = link[u'kind']
-		data = link[u'data']
-		self.Domain = data[u'domain']
-		self.Banned_by = data[u'banned_by']
-		self.Media_embed = data[u'media_embed']
-		self.Subreddit = data[u'subreddit']
-		self.Selftext_html = data[u'selftext_html']
-		self.Likes = data[u'likes']
-		self.Id = data[u'id']
-		self.Clicked = data[u'clicked']
-		self.Title = data[u'title']
-		self.Num_Comments = data[u'num_comments']
-		self.Score = data[u'score']
-		self.Approved_By = data[u'approved_by']
-		self.NSFW = data[u'over_18']
-		self.Hidden = data[u'hidden']
-		self.Thumbnail = data[u'thumbnail']
-		self.Subreddit_id = data[u'subreddit_id']
-		self.Edited = data[u'edited']
-		self.Link_flair_css_class = data[u'link_flair_css_class']
-		self.Downs = data[u'downs']
-		self.Saved = data[u'saved']
-		self.Is_self = data[u'is_self']
-		self.Permalink = data[u'permalink']
-		self.Name = data[u'name']
-		self.Created = data[u'created']
-		self.URL = data[u'url']
-		self.Author_flair_text = data[u'author_flair_text']
-		self.Author = data[u'author']
-		self.Media = data[u'media']
-		self.Num_reports = data[u'num_reports']
-		self.Ups = data[u'ups']
-		return
+    def __init__(self, link):
+        self.Kind = link[u'kind']
+        data = link[u'data']
+        self.Domain = data[u'domain']
+        self.Banned_by = data[u'banned_by']
+        self.Media_embed = data[u'media_embed']
+        self.Subreddit = data[u'subreddit']
+        self.Selftext_html = data[u'selftext_html']
+        self.Likes = data[u'likes']
+        self.Id = data[u'id']
+        self.Clicked = data[u'clicked']
+        self.Title = data[u'title']
+        self.Num_Comments = data[u'num_comments']
+        self.Score = data[u'score']
+        self.Approved_By = data[u'approved_by']
+        self.NSFW = data[u'over_18']
+        self.Hidden = data[u'hidden']
+        self.Thumbnail = data[u'thumbnail']
+        self.Subreddit_id = data[u'subreddit_id']
+        self.Edited = data[u'edited']
+        self.Link_flair_css_class = data[u'link_flair_css_class']
+        self.Downs = data[u'downs']
+        self.Saved = data[u'saved']
+        self.Is_self = data[u'is_self']
+        self.Permalink = data[u'permalink']
+        self.Name = data[u'name']
+        self.Created = data[u'created']
+        self.URL = data[u'url']
+        self.Author_flair_text = data[u'author_flair_text']
+        self.Author = data[u'author']
+        self.Media = data[u'media']
+        self.Num_reports = data[u'num_reports']
+        self.Ups = data[u'ups']
+        return
