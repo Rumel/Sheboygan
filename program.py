0d1e0048700df110b5387aff9b2ba419a657746c
Dalton Dick <daltondick2010@gmail.com>
2012-12-18 14:33:36 -0600

    Added list processing

--- program.py
+++ program.py
@@ -1,15 +1,24 @@
 import reddit
 import time
+import sys
 
-sub = raw_input("What subreddit would you like?\n")
-pages = raw_input("How many pages?\n")
+if sys.argv[1] is not None:
+    f = open(sys.argv[1], 'r')
+    lines = f.readlines()
+    for l in lines:
+        split = l.split(' ')
+        print "Downloading", split[0]
+        sub = reddit.Reddit(split[0], split[1])
+        sub.downloadImagesAsync()
+else:
+    sub = raw_input("What subreddit would you like?\n")
+    pages = raw_input("How many pages?\n")
+    sub = reddit.Reddit(sub, pages)
+    x = time.clock()
+    sub.downloadImagesAsync()
+    y = time.clock()
 
-sub = reddit.Reddit(sub, pages)
-x = time.clock()
-sub.downloadImagesAsync()
-y = time.clock()
+    print y - x
 
-print y - x
-
-print "Downloaded " + str(sub.IMGS) + " images"
-print "Downloaded " + str(sub.ALBUMS) + " albums"
+    print "Downloaded " + str(sub.IMGS) + " images"
+    print "Downloaded " + str(sub.ALBUMS) + " albums"
